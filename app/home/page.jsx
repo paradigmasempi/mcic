3fcba05f4d9a022d36f6102f43bfb80fb6b08928
IT Paradigma <admin@paraintl.com>
2025-04-08 09:07:40 +0800

    Basic Setup
    Established Footer Body and NavBar

---
+++ app/home/page.jsx
@@ -0,0 +1,12 @@
+import Homepage from "@/components/Homepage/Homepage";
+import React from "react";
+
+const page = () => {
+  return (
+    <div>
+      <Homepage />
+    </div>
+  );
+};
+
+export default page;
---
+++ components/Homepage/Homepage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Homepage = () => {
+  return (
+    <div>
+      <div>
+        <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Homepage;
